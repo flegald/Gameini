1ee51782c1cbafa6b9a6d41b68cc81ab8cb04a7d
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-05-02 13:09:25 -0700

    Download works

--- gameini/game_app/urls.py
+++ gameini/game_app/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from game_app.views import placeholder
+from game_app.views import view_file
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', placeholder)
+    url(r'^files/(?P<file_id>[0-9]+)', view_file, name='view_file'),
 ]
--- gameini/game_app/views.py
+++ gameini/game_app/views.py
@@ -1,7 +1,29 @@
+"""App views."""
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import GameModel
 
 
 def placeholder(request):
     """Place holder view."""
     return render(request, 'placeholder.html')
+
+
+def view_file(request, **kwargs):
+    """View file."""
+    # import pdb; pdb.set_trace()
+    file_id = kwargs.get('file_id')
+    file_object = GameModel.objects.filter(id=file_id).first()
+    file_name = file_object.ini_file.name
+    file = file_object.ini_file.read()
+    response = HttpResponse(file, content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename={}'.format(file_name)
+    return response
+
+
+def download_file(request, file_name):
+    """Download ini file."""
+    file = GameModel.objects.get(title=file_name).ini_file.name
+    file = file.split('/')[-1]
+    response = HttpResponse
