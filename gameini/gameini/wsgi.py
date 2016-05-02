ee293abdd5702b7f987da5179ca073f416b76751
Mike <mikekh@uw.edu>
2016-05-02 10:08:45 -0700

    Created Django app and model

---
+++ gameini/gameini/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gameini project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gameini.settings")
+
+application = get_wsgi_application()
