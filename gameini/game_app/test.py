c22a2b65a7cae09b18e9c8dbedf8ed7a418a0082
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-05-05 17:46:35 -0700

    David Flegal more view tests including file downloaded test.

--- gameini/game_app/test.py
+++ gameini/game_app/test.py
@@ -1,10 +1,6 @@
-"""Tests file."""
+"""Tests Views and Model."""
 from django.test import TestCase, Client
 from .models import GameModel
-from .forms import UploadForm
-import factory
-import os
-from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 class GameTestCase(TestCase):
@@ -18,53 +14,56 @@ class GameTestCase(TestCase):
 
     # Model Tests
 
-    # def test_game_title_works(self):
-    #     """Assert game has title."""
-    #     self.assertTrue(self.file.title)
+    def test_game_title_works(self):
+        """Assert game has title."""
+        self.assertTrue(self.file.title)
 
-    # def test_game_file_works(self):
-    #     """Assert game has file."""
-    #     self.assertTrue(self.file.ini_file)
+    def test_game_file_works(self):
+        """Assert game has file."""
+        self.assertTrue(self.file.ini_file)
 
-    # def test_in_db(self):
-    #     """Assert files in DB."""
-    #     self.assertEqual(len(GameModel.objects.all()), 1)
+    def test_in_db(self):
+        """Assert files in DB."""
+        self.assertEqual(len(GameModel.objects.all()), 1)
 
-    # # View Tests
+    # View Tests
 
-    # def test_home_view_get(self):
-    #     """Test home view."""
-    #     self.assertEqual(self.client.get('/').status_code, 200)
+    # Home Views tests
+    def test_home_view_get(self):
+        """Test home view with GET request."""
+        self.assertEqual(self.client.get('/').status_code, 200)
 
+    def test_home_view_post(self):
+        """Test home view with POST request."""
+        file = GameModel(title='grass cutters 5', ini_file='ini_files/settings.ini')
+        file.save()
+        key = file.pk
+        response = self.client.post(
+            '/',
+            {'games': key}
+        )
+        self.assertRedirects(response, '/generateform/{}'.format(key), status_code=302)
+
+    # Test Upload View, view only accepts POST
     def test_upload_view_post(self):
         """Test upload view post."""
-
         file = GameModel(title='grass cutters 4', ini_file='ini_files/settings.ini')
-
         response = self.client.post(
-
             '/files/upload',
             {'title': file.title, 'ini_file': file.ini_file},
             format='multipart',
             follow=True
         )
-
         file = GameModel.objects.get(title='grass cutters 4')
         self.assertEqual(response.status_code, 200)
         self.assertRedirects(response, '/generateform/{}'.format(file.id), status_code=302)
 
-    # def test_upload_view(self):
-    #     """Test upload view."""
-    #     response = self.client.post(
-    #         '/files/upload',
-    #         {'title': self.file.title,
-    #             'ini_file': self.file.ini_file},
-    #         follow=True
-    #     )
-    #     import pdb; pdb.set_trace()
-    #     self.assertEqual(response[0], '/generateform/{}'.format(self.file.id))
+    # Test Generate Form View, view only accepts POST and returns a downloadable file.
+    def test_generate_form_with_download(self):
+        """Test that a file is handed back to the user as a download."""
+        file = GameModel(title='grass cutters 4', ini_file='ini_files/settings.ini')
+        file.save()
+        response = self.client.post('/generateform/{}'.format(file.pk))
+        self.assertEqual(response.get('Content-Disposition'), "attachment; filename={}.ini".format(file.title))
 
-    # def test_generate_form(self):
-    #     """Test Generate Form."""
-    #     response = self.client.post('/generateform/{}'.format(self.file.id), self.file.ini_file)
-    #     self.assertEqual(self.client.get(response.status_code, 200))
+
