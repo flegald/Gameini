788c5c14f478e69784bd6c439637d57b04954f23
Mike <mikekh@uw.edu>
2016-05-05 17:40:29 -0700

    Added len test to reset file

--- gameini/game_app/test_junk.py
+++ gameini/game_app/test_junk.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from .models import GameModel
-from .forms import UploadForm
-from game_app.junk_drawer import reset_file
-import os
+from game_app.junk_drawer import reset_file, config_section_map
 
 
 class JunkTestCase(TestCase):
@@ -10,9 +8,21 @@ class JunkTestCase(TestCase):
 
     def setUp(self):
         """Setup."""
+        self.model = GameModel(title='test1', ini_file='ini_files/UserSettings.ini')
 
-    def test_reset_file(self):
-        """Test Reset."""
-        model = GameModel(title='test1', ini_file='ini_files/UserSettings.ini')
-        file = reset_file(model.ini_file)
+    def test_reset_file_is_str(self):
+        """Test reset file produces a str object."""
+        file = reset_file(self.model.ini_file)
         self.assertIsInstance(file, str)
+
+    def test_rest_file_is_at_begining(self):
+        """Test file has a length greater than 0."""
+        file = reset_file(self.model.ini_file)
+        self.assertGreater(len(file), 0)
+        
+
+    # def test_config_section_map(self):
+    #     """Test Config section map."""
+    #     parsed_dict = config_section_map(self.model.ini_file)
+    #     import pdb; pdb.set_trace
+    #     self.assertIsInstance(parsed_dict, dict)
