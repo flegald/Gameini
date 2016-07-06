2e6d0229e0dc4b8fe7387df9eeaf2b9b57d2455c
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-07-05 21:14:03 -0700

    Modify allowed hosts again

--- gameini/game_app/settings.py
+++ gameini/game_app/settings.py
@@ -32,11 +32,7 @@ SECRET_KEY = 'rr11tt4ygv4-90kkp@=rgotx+8ci-jsn5e=rviw27hoo)#neqc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-                    'localhost',
-                    'ec2-52-34-232-173.us-west-2.compute.amazonaws.com',
-                    'https://gameini.herokuapp.com',
-                ]
+ALLOWED_HOSTS = ['localhost', 'ec2-52-34-232-173.us-west-2.compute.amazonaws.com', '.herokuapp.com']
 
 
 # Application definition
